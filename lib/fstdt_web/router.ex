2bb0ca63ae0a35aa630e67b8c66c290fca1f261d
Michael Howell <michael@notriddle.com>
2017-09-14 18:58:37 -0700

    Write up a "draft" of the new-style URLs

--- lib/fstdt_web/router.ex
+++ lib/fstdt_web/router.ex
@@ -17,6 +17,56 @@ defmodule FstdtWeb.Router do
     pipe_through :browser
 
     get "/", IndexController, :index
+    get "/search", SearchController, :search
+  end
+
+  # Example URLs:
+  # - http://fstdt.org/l/fstdt/commented/
+  #   Show fundamentalist quotes starting from the most recently commented
+  # - http://fstdt.org/l/cstdt/2016/01/
+  #   For January of 2016, show Conspiracy quotes in reverse-chronological order
+  # - http://fstdt.org/l/cstdt/2016/01/
+  #   For January of 2016, show Conspiracy quotes in reverse-chronological order
+  # - http://fstdt.org/l/
+  #   Show all topics (currently FSTDT, CSTDT, RSTDT, and SSTDT)
+  scope "/l/", FstdtWeb do
+    pipe_through :browser
+
+    get "/", QuoteListController, :index
+    get "/:topic/", QuoteListController, :show_new
+    get "/:topic/:year/", QuoteListController, :dates
+    get "/:topic/:year/:month", QuoteListController, :show_old
+    get "/:topic/commented", QuoteListController, :show_recently_commented
+    get "/:topic/random", QuoteListController, :show_random
+    get "/:topic/top", QuoteListController, :show_top
+  end
+
+  scope "/submit/", FstdtWeb do
+    get "/", QuoteSubmitController, :index
+    # The :submission ID is a UUID generated as part of the submit form.
+    # It is used to detect duplicate submissions,
+    # and does not need to be kept forever.
+    # No error should be shown to the user if a duplicate is detected;
+    # if their connection is flaky, that's fine.
+    put "/:submission", QuoteSubmitController, :submit
+  end
+
+  scope "/q/", FstdtWeb do
+    pipe_through :browser
+
+    get "/:quote/", QuotePageController, :show
+    put "/:quote/comments/:submission", CommentSubmitController, :submit
+  end
+
+  scope "/admin/", FstdtWeb do
+    pipe_through :browser
+
+    get "/", PubAdminController, :index
+    post "/", PubAdminController, :review
+    put "/q/:quote", QuoteAdminController, :edit
+    delete "/q/:quote", QuoteAdminController, :delete
+    put "/c/:comment", CommentAdminController, :edit
+    delete "/c/:comment", CommentAdminController, :delete
   end
 
   # FSTDT is linked to. A lot. We are *not* going to break those old links.
