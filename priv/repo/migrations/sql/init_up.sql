9f83ba6b3de0552076f2f17bd3fe0429ee9bd713
Spike Dee <spikedee@openmailbox.org>
2017-11-02 19:00:36 -0500

    updated to contain core functionality of old db

--- priv/repo/migrations/sql/init_up.sql
+++ priv/repo/migrations/sql/init_up.sql
@@ -1,26 +1,122 @@
-CREATE TABLE users (
-  user_id SERIAL PRIMARY KEY,
-  name VARCHAR(1024) NOT NULL
+
+-- table: ip_addresses
+--    this will collect ip addresses from 
+--    submissions, quotes, comments, registrations, etc.
+
+create table ip_addresses (
+    id                  bigserial not null primary key,
+    address             character (16) unique not null
 );
 
-CREATE TABLE fundies (
-  fundie_id SERIAL PRIMARY KEY,
-  name VARCHAR(1024) NOT NULL
+-- table: banned_IPs
+--     contains banned ip addresses and address ranges
+--     supports wildcards ? and *
+--     set date expires to 0000/00/00 00:00 for permaban ?
+
+create table banned_IPs (
+    id                  bigserial primary key,
+    address             character (16) unique not null,
+    date_banned         timestamp without time zone not null,
+    date_expires        timestamp without time zone not null,
+    banned_by           bigint references users (id),
+    remarks             character varying (512)
 );
 
-CREATE TABLE boards (
-  board_id SERIAL PRIMARY KEY,
-  name VARCHAR(1024) NOT NULL
+-- table: users
+--
+--    since users can enter a name without being registered,
+--    this table is somewhat weird and a not of fields are missing "not null"s
+--    because they don't apply to unregistered users.
+--
+--    should we split this table?
+--    id 0 := anonymous?
+--
+--    also, account types:
+--    0: locked / banned username
+--    1: unregistered user
+--    2: registered user 
+--    3: registered user with pubadmin access
+--    4: mod
+--    5: admin
+
+create table users (
+    id                  bigserial primary key,
+    username            character varying (128) not null,
+    is_registered       boolean not null,
+    date_registered     timestamp without time zone,
+    email               character varying (512),
+    registration_ip     bigint not null references ip_addresses (id),
+    pw_hash             character (512),
+    pw_salt             character (512),
+    account_type        smallint not null,
+    submission_count    bigint not null,
+    comment_count       bigint not null,
+    unique (username, is_registered)
 );
 
-CREATE TABLE quotes (
-  quote_id SERIAL PRIMARY KEY,
-  text_input_type INT NOT NULL DEFAULT 1,
-  text_input TEXT NOT NULL,
-  text_cooked_html TEXT NULL DEFAULT NULL,
-  submitter_user_id INT NOT NULL REFERENCES users,
-  fundie_id INT NOT NULL REFERENCES fundies,
-  board_id INT NOT NULL REFERENCES boards,
-  url TEXT NOT NULL
+create table fundies (
+    id                  bigserial primary key,
+    name                character varying (128) not null unique,
+    nlzd_name           character varying (128) not null
 );
-COMMENT ON COLUMN quotes.text_input_type IS '0=PLAINTEXT,1=GFM,2=YSCODE';
+
+create table sites (
+    id                  bigserial primary key,
+    name                character varying (128) not null unique,
+    nlzd_name           character varying (128) not null
+);
+
+create table categories (
+    id                  smallserial primary key,
+    name                character varying (64) not null unique,
+    abbreviation        character varying (8) not null unique,
+    description         character varying (128) not null unique
+);
+
+create table quotes (
+    id                  bigserial primary key,
+    date_posted         timestamp without time zone not null,
+    submitter           bigint references users (id) not null,
+    submitter_ip        bigint references ip_addresses (id) not null,
+    fundie              bigint references fundies (id) not null,
+    site                bigint references sites (id) not null,
+    text                character varying (65536) not null,
+    votes_yay           bigint not null,
+    votes_nay           bigint not null,
+    unique_views        bigint not null,
+    total_views         bigint not null,
+    is_visible          boolean not null,
+    is_locked           boolean not null,
+    use_yscodes         boolean not null
+);
+
+create table quotes_categories_link (
+    quote_id            bigint references quotes (id) not null,
+    category_id         bigint references categories (id) not null,
+    primary key (quote_id, category_id)
+);
+
+create table comments (
+    id                  bigserial primary key,
+    quote               bigint references quotes (id),
+    author              bigint references users (id),
+    ip_address          bigint references ip_addresses (id),
+    date_posted         timestamp without time zone not null,
+    date_modified       timestamp without time zone,
+    text                character varying (65536) not null,     
+    is_visible          boolean not null
+);
+
+create table stats_useragents (
+    id                  BIGSERIAL primary key,
+    string              character varying (512) not null unique
+);
+
+create table stats_displays (
+    id                  bigserial primary key,
+    size_small          integer not null,
+    size_large          integer not null,
+    is_landscape        integer not null,
+    is_portrait         integer not null,
+    unique (size_small, size_large)
+);
